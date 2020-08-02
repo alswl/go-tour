0b554178e02ed99ec3ff5b348d254e409a8f323a
alswl <alswlx@gmail.com>
2020-08-02 23:33:03 +0800

    .

---
+++ methods/7-indirection-values.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex)Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func AbsFunc(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := Vertex{X: 3, Y: 4}
+	fmt.Println(v.Abs())
+	fmt.Println(AbsFunc(v))
+
+	p := &Vertex{3, 4}
+	// In this case, the method call p.Abs() is interpreted as (*p).Abs().
+	fmt.Println(p.Abs())
+	fmt.Println(AbsFunc(*p))
+
+}
